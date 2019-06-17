e5aa01486246ee8e79b10a5b694937726c9a0d0b
ricenshine24 <jon_rice@carmax.com>
2019-06-17 15:34:38 -0400

    Push new

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int id { get; set; }
+        [Required]
+        [MaxLength(5)]
+        public string description { get; set; }
+
+    }
+}
