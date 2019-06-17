48a58822f08e09a36ba1b1cd978a04f4889fb34a
ricenshine24 <jon_rice@carmax.com>
2019-06-17 14:25:44 -0400

    Push #2 for 1st round of changes

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -22,12 +22,7 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            /*
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-            */
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
