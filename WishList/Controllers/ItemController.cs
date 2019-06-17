d5c12e6da51418f6ebb92255f1344c2ddb9ca2a1
ricenshine24 <jon_rice@carmax.com>
2019-06-17 16:13:53 -0400

    added HttpPost

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -32,6 +32,7 @@ namespace WishList.Controllers
             return View("Create");
         }
 
+        [HttpPost]
         public IActionResult Create(Models.Item item)
         {
             _context.Items.Add(item);
